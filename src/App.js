d5fd8a99734aa9633ebb251eee552ec33ac3f45c
Debjit Majumder <majumderdebjit@gmail.com>
2022-07-29 22:47:25 +0530

    url validatior application

--- src/App.js
+++ src/App.js
@@ -1,24 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import axios from "axios";
+import { TextField, LinearProgress } from "@mui/material";
+import "./App.css";
 
 function App() {
+  const [value, setValue] = useState("");
+  const [valid, setValid] = useState([]);
+  const [invalid, setInvalid] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [count, setCount] = useState(0);
+  const [total, setTotal] = useState(0);
+
+  const handleValidate = async () => {
+    if (value.trim().length === 0) return;
+
+    const urls = value.split("\n").filter((url) => url.trim() !== "");
+
+    if (urls.length === 0) return;
+
+    setLoading(true);
+    setValid([]);
+    setInvalid([]);
+    setCount(0);
+    setTotal(urls.length);
+
+    for (let url of urls) {
+      try {
+        const response = await axios.get(url);
+        if (String(response.status).startsWith("2"))
+          setValid((prev) => [...prev, url]);
+        else throw new Error();
+      } catch (error) {
+        setInvalid((prev) => [...prev, url]);
+      }
+
+      setCount((prev) => prev + 1);
+    }
+    setLoading(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="main-content">
+        <h1>URL Validator</h1>
+        <TextField
+          style={{
+            width: "100%",
+            background: "white",
+            marginTop: "2rem",
+          }}
+          multiline
+          rows={10}
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <div style={{ marginTop: "15px" }}>
+          <button
+            className="validate-btn"
+            disabled={value.trim().length === 0 || loading}
+            onClick={handleValidate}
+          >
+            VALIDATE
+          </button>
+        </div>
+      </div>
+
+      {!!total && (
+        <div className="progress">
+          <LinearProgress
+            style={{ marginTop: "1rem" }}
+            sx={{
+              "& .MuiLinearProgress-bar": {
+                transition: "none",
+              },
+            }}
+            variant="determinate"
+            value={Math.round((count * 100) / total)}
+          />
+          <h4 className="progress-info">
+            ({count}/{total}) URLs checked
+          </h4>
+        </div>
+      )}
+
+      {(!!valid.length || !!invalid.length) && (
+        <div className="results">
+          <div className="valid">
+            <h3>Valid URLs</h3>
+            <ul>
+              {valid.map((url, index) => (
+                <li className="item" key={index}>
+                  {url}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="invalid">
+            <h3>Invalid URLs</h3>
+            <ul>
+              {invalid.map((url, index) => (
+                <li className="item" key={index}>
+                  {url}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
